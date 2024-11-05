92877f9c0104da6301a407329659cb1c539d7fd9
Rafael Neves <nevesrafael.dev@gmail.com>
2024-11-05 13:46:12 -0300

    :sparkles: Feat shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-zinc-800">
-      Hello World!
-    </div>
+    <>
+      <Button>Hello World</Button>
+    </>
   );
 }
